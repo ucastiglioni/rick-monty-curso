9f9cc2bd53b1c75ca11ac1478e119b6fb9cef7b9
ucastiglioni <ucastiglioni@gmail.com>
2023-04-12 19:06:09 -0300

    First commit course - Maxi

---
+++ src/components/Characters.js
@@ -0,0 +1,52 @@
+export default function Characters(props) {
+  console.log(props);
+  const { characters, setCharacters } = props;
+    const resetCharacters=()=>{
+        console.log("reseteando char");
+        setCharacters(null);
+
+    }
+  return (
+    <div className="characters">
+      <h1>Personajes</h1>
+
+      <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
+      <div className="container-characters">
+        {characters.map((character, index) => (
+          <div className="character-container" key={index}>
+            <div>
+              <img src={character.image} alt={character.name} />
+            </div>
+            <div>
+              <h3>{character.name}</h3>
+              <h6>
+                {character.status === "Alive" ? (
+                  <>
+                    <span className="alive" />
+                    Alive
+                  </>
+                ) : (
+                  <>
+                    <span className="dead" />
+                    Dead
+                  </>
+                )}
+              </h6>
+              <p>
+                <span className="text-grey">Episodios: </span>
+                <span>{character.episode.length}</span>
+              </p>
+              <p>
+                <span className="text-grey">Especie: </span>
+                <span>{character.species}</span>
+              </p>
+            </div>
+            {/* <p>{character.name}</p> */}
+          </div>
+        ))}
+      </div>
+      <span className="back-home" onClick={resetCharacters}>Volver a la home</span>
+    </div>
+    
+  );
+}
